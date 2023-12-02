f33e3005970319dfb76051014c7c25d5f57b8d12
Thbop <75861321+thbop@users.noreply.github.com>
2023-12-02 18:10:32 -0500

    Upload files

---
+++ main.py
@@ -0,0 +1,51 @@
+from PIL import Image
+from pygame.math import Vector3
+
+base_image = Image.open('heightmap.png')
+pix = base_image.load()
+
+normal_image = Image.new('RGB', base_image.size)
+npix = normal_image.load()
+
+
+def rgb_to_v(color):
+    return color[0] * 0.2126 + color[1] * 0.7152 + color[2] * 0.0722
+
+def v_to_color(v):
+    return max(min(round(v * 255), 255), 0)
+
+def nvec_to_color(vec):
+    return (
+        v_to_color(vec.x),
+        v_to_color(vec.y),
+        v_to_color(vec.z)
+    )
+
+for x in range(base_image.size[0]):
+    for y in range(base_image.size[1]):
+        border_pixel = x == 0 or x == base_image.size[0] - 1 or y == 0 or y == base_image.size[1] - 1
+        if not border_pixel:
+            v1 = rgb_to_v(pix[x, y - 1])
+            v2 = rgb_to_v(pix[x + 1, y])
+            v3 = rgb_to_v(pix[x, y + 1])
+            v4 = rgb_to_v(pix[x - 1, y])
+            
+            normal = Vector3(
+                -(v4 - v2) * ((y - 1) - (y + 1)),
+                -((x - 1) - (x + 1)) * (v1 - v3),
+                ((x - 1) - (x + 1)) - ((y - 1) - (y + 1))
+            )
+            if normal.length() != 0:
+                normal = normal.normalize()
+            else:
+                normal = Vector3(0, 1, 0)
+
+            # Lambertian
+            # value = v_to_color( normal.dot(Vector3(.5, -1, 0)) ) 
+            # npix[x, y] = (value, value, value)
+            
+            # Just normal
+            npix[x, y] = nvec_to_color(normal)
+
+
+normal_image.save('normalmap.png')
